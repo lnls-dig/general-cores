77066de4b5cd896d18868ed566a68e3ead4cfa14
Tomasz Wlostowski <tomasz.wlostowski@cern.ch>
2011-04-29 16:49:09 +0200

    modules/common: added hdlmake Manifest.

---
+++ modules/common/Manifest.py
@@ -0,0 +1,8 @@
+files = [	"gencores_pkg.vhd",
+					"gc_crc_gen.vhd",
+					"gc_moving_average.vhd",
+					"gc_extend_pulse.vhd",
+					"gc_delay_gen.vhd",
+					"gc_dual_pi_controller.vhd",
+					"gc_serial_dac.vhd",
+					"gc_sync_ffs.vhd"];
