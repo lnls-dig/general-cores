0a2cf0523f753cffbf6554727f19403bceaf7231
Tomasz Wlostowski <tomasz.wlostowski@cern.ch>
2011-10-04 13:54:28 +0200

    wishbone: moved wb_slave_adapter to separate subdir

--- modules/wishbone/Manifest.py
+++ modules/wishbone/Manifest.py
@@ -1,17 +1,21 @@
 modules =  { "local" :
 	
 						[ 
-#						"wb_async_bridge",
+						"wb_async_bridge",
 						"wb_onewire_master",
 						"wb_i2c_master",
 						"wb_bus_fanout",
 						"wb_conmax",
+						"wb_dpram",
 						"wb_gpio_port",
 						"wb_simple_timer",
 						"wb_uart",
 						"wb_vic",
 						"wb_spi",
 						"wb_virtual_uart",
+						"wb_crossbar",
+						"wb_lm32",
+						"wb_slave_adapter",
 						"wbgen2"
 						 ]};
 
