d115a4a142909322d83f3d10822bdcbf09d06982
Tomasz Wlostowski <tomasz.wlostowski@cern.ch>
2011-10-05 15:53:46 +0200

    wishbone: wb_i2c_master: added WB slave adapter and xwb_ wrapper

--- modules/wishbone/wb_i2c_master/xwb_i2c_master.vhd
+++ modules/wishbone/wb_i2c_master/xwb_i2c_master.vhd
@@ -5,7 +5,8 @@ use work.wishbone_pkg.all;
 
 entity xwb_i2c_master is
   generic(
-    g_interface_mode         : t_wishbone_interface_mode := CLASSIC
+    g_interface_mode      : t_wishbone_interface_mode      := CLASSIC;
+    g_address_granularity : t_wishbone_address_granularity := WORD
     );
   port (
     clk_sys_i : in std_logic;
@@ -26,21 +27,23 @@ end xwb_i2c_master;
 
 architecture rtl of xwb_i2c_master is
 
-  component i2c_master_top
+  component wb_i2c_master
     generic (
-      ARST_LVL : std_logic);
+      g_interface_mode      : t_wishbone_interface_mode;
+      g_address_granularity : t_wishbone_address_granularity);
     port (
-      wb_clk_i     : in  std_logic;
-      wb_rst_i     : in  std_logic := '0';
-      arst_i       : in  std_logic := not ARST_LVL;
-      wb_adr_i     : in  std_logic_vector(2 downto 0);
-      wb_dat_i     : in  std_logic_vector(7 downto 0);
-      wb_dat_o     : out std_logic_vector(7 downto 0);
-      wb_we_i      : in  std_logic;
+      clk_sys_i    : in  std_logic;
+      rst_n_i      : in  std_logic;
+      wb_adr_i     : in  std_logic_vector(4 downto 0);
+      wb_dat_i     : in  std_logic_vector(31 downto 0);
+      wb_dat_o     : out std_logic_vector(31 downto 0);
+      wb_sel_i     : in  std_logic_vector(3 downto 0);
       wb_stb_i     : in  std_logic;
       wb_cyc_i     : in  std_logic;
+      wb_we_i      : in  std_logic;
       wb_ack_o     : out std_logic;
-      wb_inta_o    : out std_logic;
+      wb_int_o     : out std_logic;
+      wb_stall_o   : out std_logic;
       scl_pad_i    : in  std_logic;
       scl_pad_o    : out std_logic;
       scl_padoen_o : out std_logic;
@@ -48,45 +51,32 @@ architecture rtl of xwb_i2c_master is
       sda_pad_o    : out std_logic;
       sda_padoen_o : out std_logic);
   end component;
-
-  signal rst     : std_logic;
-  signal dat_out : std_logic_vector(7 downto 0);
   
 begin  -- rtl
 
 
-  gen_test_mode : if(g_interface_mode /= CLASSIC) generate
-
-    assert false report "xwb_i2c_master: this module can only work with CLASSIC wishbone interface" severity failure;
-
-  end generate gen_test_mode;
-  
-  rst <= not rst_n_i;
-
-  Wrapped_I2C : i2c_master_top
+  U_Wrapped_I2C : wb_i2c_master
     generic map (
-      ARST_LVL => '0')
+      g_interface_mode      => g_interface_mode,
+      g_address_granularity => g_address_granularity)
     port map (
-      wb_clk_i     => clk_sys_i,
-      wb_rst_i     => rst,
-      arst_i       => '1',
-      wb_adr_i     => slave_i.adr(2 downto 0),
-      wb_dat_i     => slave_i.dat(7 downto 0),
-      wb_dat_o     => dat_out,
-      wb_we_i      => slave_i.we,
+      clk_sys_i    => clk_sys_i,
+      rst_n_i      => rst_n_i,
+      wb_adr_i     => slave_i.adr(4 downto 0),
+      wb_dat_i     => slave_i.dat,
+      wb_dat_o     => slave_o.dat,
+      wb_sel_i     => slave_i.sel,
       wb_stb_i     => slave_i.stb,
       wb_cyc_i     => slave_i.cyc,
+      wb_we_i      => slave_i.we,
       wb_ack_o     => slave_o.ack,
-      wb_inta_o    => slave_o.int,
+      wb_int_o     => slave_o.int,
+      wb_stall_o   => slave_o.stall,
       scl_pad_i    => scl_pad_i,
       scl_pad_o    => scl_pad_o,
       scl_padoen_o => scl_padoen_o,
       sda_pad_i    => sda_pad_i,
       sda_pad_o    => sda_pad_o,
       sda_padoen_o => sda_padoen_o);
-
-  slave_o.dat(7 downto 0)                <= dat_out;
-  slave_o.dat(slave_o.dat'left downto 8) <= (others => '0');
-
 end rtl;
 
