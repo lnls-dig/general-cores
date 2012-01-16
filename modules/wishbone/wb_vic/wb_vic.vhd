10e213b5b915726d357fb6fc73ea9fb98b7bcc77
Tomasz Włostowski <tomasz.wlostowski@cern.ch>
2012-01-17 00:17:15 +0100

    modules/wb_vic: fixed port names to match the coding style

--- modules/wishbone/wb_vic/wb_vic.vhd
+++ modules/wishbone/wb_vic/wb_vic.vhd
@@ -5,7 +5,7 @@
 -- Author     : Tomasz Wlostowski
 -- Company    : CERN BE-Co-HT
 -- Created    : 2010-05-18
--- Last update: 2010-07-29
+-- Last update: 2012-01-13
 -- Platform   : FPGA-generic
 -- Standard   : VHDL'87
 -------------------------------------------------------------------------------
@@ -38,25 +38,29 @@ use work.wishbone_pkg.all;
 entity wb_vic is
   
   generic (
+    g_interface_mode      : t_wishbone_interface_mode      := CLASSIC;
+    g_address_granularity : t_wishbone_address_granularity := WORD;
+
     g_num_interrupts : natural := 32    -- number of IRQ inputs.
     );
 
   port (
+    clk_sys_i : in std_logic;            -- wishbone clock
     rst_n_i  : in std_logic;            -- reset
-    wb_clk_i : in std_logic;            -- wishbone clock
 
-    wb_addr_i : in  std_logic_vector(5 downto 0);
-    wb_data_i : in  std_logic_vector(31 downto 0);
-    wb_data_o : out std_logic_vector(31 downto 0);
-    wb_cyc_i  : in  std_logic;
-    wb_sel_i  : in  std_logic_vector(3 downto 0);
-    wb_stb_i  : in  std_logic;
-    wb_we_i   : in  std_logic;
-    wb_ack_o  : out std_logic;
+    wb_adr_i : in  std_logic_vector(c_wishbone_address_width-1 downto 0);
+    wb_dat_i : in  std_logic_vector(c_wishbone_data_width-1 downto 0);
+    wb_dat_o : out std_logic_vector(c_wishbone_data_width-1 downto 0);
+    wb_cyc_i : in  std_logic;
+    wb_sel_i : in  std_logic_vector(c_wishbone_data_width/8-1 downto 0);
+    wb_stb_i : in  std_logic;
+    wb_we_i  : in  std_logic;
+    wb_ack_o : out std_logic;
+    wb_stall_o : out std_logic;
 
     irqs_i       : in  std_logic_vector(g_num_interrupts-1 downto 0);  -- IRQ inputs
-    irq_master_o : out std_logic        -- master IRQ output (multiplexed line,
-                                        -- to the CPU)
+    irq_master_o : out std_logic        -- master IRQ output (multiplexed line, to the CPU)
+             
     );
 
 end wb_vic;
@@ -128,6 +132,10 @@ architecture syn of wb_vic is
   signal current_irq    : std_logic_vector(4 downto 0);
   signal irq_id_encoded : std_logic_vector(4 downto 0);
   signal state          : t_state;
+
+    signal wb_in  : t_wishbone_slave_in;
+  signal wb_out : t_wishbone_slave_out;
+
   
 begin  -- syn
 
@@ -136,9 +144,9 @@ begin  -- syn
   end generate check1;
 
 
-  register_irq_lines : process(wb_clk_i, rst_n_i)
+  register_irq_lines : process(clk_sys_i, rst_n_i)
   begin
-    if rising_edge(wb_clk_i) then
+    if rising_edge(clk_sys_i) then
       if rst_n_i = '0' then
         irqs_i_reg <= (others => '0');
       else
@@ -160,18 +168,47 @@ begin  -- syn
 
   vic_ivt_ram_addr <= current_irq;
 
-  wb_controller : wb_slave_vic
+  U_Slave_adapter: wb_slave_adapter
+    generic map (
+      g_master_use_struct  => true,
+      g_master_mode        => CLASSIC,
+      g_master_granularity => WORD,
+      g_slave_use_struct   => false,
+      g_slave_mode         => g_interface_mode,
+      g_slave_granularity  => g_address_granularity)
+    port map (
+      clk_sys_i => clk_sys_i,
+      rst_n_i    => rst_n_i,
+      sl_adr_i   => wb_adr_i,
+      sl_dat_i   => wb_dat_i,
+      sl_sel_i   => wb_sel_i,
+      sl_cyc_i   => wb_cyc_i,
+      sl_stb_i   => wb_stb_i,
+      sl_we_i    => wb_we_i,
+      sl_dat_o   => wb_dat_o,
+      sl_ack_o   => wb_ack_o,
+      sl_stall_o => wb_stall_o,
+      master_i   => wb_out,
+      master_o   => wb_in);
+
+  wb_out.stall <= '0';
+  wb_out.rty <= '0';
+  wb_out.err <= '0';
+  wb_out.int <= '0';
+
+  
+  U_wb_controller : wb_slave_vic
     port map (
       rst_n_i   => rst_n_i,
-      wb_clk_i  => wb_clk_i,
-      wb_addr_i => wb_addr_i,
-      wb_data_i => wb_data_i,
-      wb_data_o => wb_data_o,
-      wb_cyc_i  => wb_cyc_i,
-      wb_sel_i  => wb_sel_i,
-      wb_stb_i  => wb_stb_i,
-      wb_we_i   => wb_we_i,
-      wb_ack_o  => wb_ack_o,
+      wb_clk_i  => clk_sys_i,
+      wb_addr_i => wb_in.adr(5 downto 0),
+      wb_data_i => wb_in.dat,
+      wb_data_o => wb_out.dat,
+      wb_cyc_i  => wb_in.cyc,
+      wb_sel_i  => wb_in.sel,
+      wb_stb_i  => wb_in.stb,
+      wb_we_i   => wb_in.we,
+      wb_ack_o  => wb_out.ack,
 
       vic_ctl_enable_o   => vic_ctl_enable,
       vic_ctl_pol_o      => vic_ctl_pol,
@@ -190,9 +227,9 @@ begin  -- syn
       vic_ivt_ram_data_o => vic_ivt_ram_data,
       vic_ivt_ram_rd_i   => vic_ivt_ram_rd);
 
-  process (wb_clk_i, rst_n_i)
+  process (clk_sys_i, rst_n_i)
   begin  -- process enable_disable_irqs
-    if rising_edge(wb_clk_i) then
+    if rising_edge(clk_sys_i) then
       
       if rst_n_i = '0' then             -- asynchronous reset (active low)
         vic_imr <= (others => '0');
@@ -218,9 +255,9 @@ begin  -- syn
     end if;
   end process;
 
-  vic_fsm : process (wb_clk_i, rst_n_i)
+  vic_fsm : process (clk_sys_i, rst_n_i)
   begin  -- process vic_fsm
-    if rising_edge(wb_clk_i) then
+    if rising_edge(clk_sys_i) then
       
       if rst_n_i = '0' then             -- asynchronous reset (active low)
         state          <= WAIT_IRQ;
