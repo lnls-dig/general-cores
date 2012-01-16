5fa88a37ac3c00787fef7819c9b413f55e9e978d
Tomasz Włostowski <tomasz.wlostowski@cern.ch>
2012-01-17 00:11:33 +0100

    modules/common: updated manifest and package with freshly added modules

--- modules/common/Manifest.py
+++ modules/common/Manifest.py
@@ -1,8 +1,10 @@
 files = [	"gencores_pkg.vhd",
-					"gc_crc_gen.vhd",
-					"gc_moving_average.vhd",
-					"gc_extend_pulse.vhd",
-					"gc_delay_gen.vhd",
-					"gc_dual_pi_controller.vhd",
-					"gc_serial_dac.vhd",
-					"gc_sync_ffs.vhd"];
+                "gc_crc_gen.vhd",
+                "gc_moving_average.vhd",
+                "gc_extend_pulse.vhd",
+                "gc_delay_gen.vhd",
+                "gc_dual_pi_controller.vhd",
+                "gc_serial_dac.vhd",
+                "gc_sync_ffs.vhd",
+                "gc_pulse_synchronizer.vhd",
+                "gc_frequency_meter.vhd"];
