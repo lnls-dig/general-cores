2e5c4532450def61962d0298ab49e344b8db8013
Tomasz Włostowski <tomasz.wlostowski@cern.ch>
2012-01-31 16:04:40 +0100

    wishbone: added Xilinx FPGA serial loader (wb_xilinx_fpga_loader)

--- modules/wishbone/Manifest.py
+++ modules/wishbone/Manifest.py
@@ -1,11 +1,9 @@
 modules =  { "local" :
-	
 						[ 
 						"wb_async_bridge",
 						"wb_onewire_master",
 						"wb_i2c_master",
 						"wb_bus_fanout",
-#						"wb_conmax",
 						"wb_dpram",
 						"wb_gpio_port",
 						"wb_simple_timer",
@@ -15,6 +13,7 @@ modules =  { "local" :
 						"wb_crossbar",
 						"wb_lm32",
 						"wb_slave_adapter",
+						"wb_xilinx_fpga_loader",
 						"wbgen2"
 						 ]};
 
