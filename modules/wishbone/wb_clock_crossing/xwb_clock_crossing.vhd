6c455d75d186835ccbca252fe3ee4f44c3c94c49
Wesley W. Terpstra <w.terpstra@gsi.de>
2012-03-15 10:40:05 +0100

    Before the output CYC line was always high. That breaks devices (like the crossbar) which use that to signal end of transation. Thus:   Include the cycle line in the clock crossing.   Whenever the cycle drops, queue this transition to the FIFO.

--- modules/wishbone/wb_clock_crossing/xwb_clock_crossing.vhd
+++ modules/wishbone/wb_clock_crossing/xwb_clock_crossing.vhd
@@ -22,7 +22,9 @@ entity xwb_clock_crossing is
 end xwb_clock_crossing;
 
 architecture rtl of xwb_clock_crossing is
-   constant mWE_start  : natural := 0;
+   constant mCYC_start : natural := 0;
+   constant mCYC_end   : natural := mCYC_start;
+   constant mWE_start  : natural := mCYC_end + 1;
    constant mWE_end    : natural := mWE_start;
    constant mADR_start : natural := mWE_end + 1;
    constant mADR_end   : natural := mADR_start + c_wishbone_address_width - 1;
@@ -50,6 +52,7 @@ architecture rtl of xwb_clock_crossing is
    signal ssend_vect, srecv_vect : std_logic_vector(slen-1 downto 0);
    signal sw_rdy, sw_en, sr_rdy, sr_en, sa_rdy, sa_en : std_logic;
    
+   signal slave_CYC : std_logic;
    signal master_o_STB : std_logic;
    signal slave_o_PUSH : std_logic;
 begin
@@ -59,12 +62,14 @@ begin
                r_clk_i => master_clk_i, r_rdy_o => mr_rdy, r_en_i => mr_en, r_data_o => mrecv_vect,
                a_clk_i => '0', a_rdy_o => open, a_en_i => '0', rst_n_i => rst_n_i);
 
+   msend_vect(mCYC_start) <= msend.CYC;
    msend_vect(mWE_start) <= msend.WE;
    msend_vect(mADR_end downto mADR_start) <= msend.ADR;
    msend_vect(mDAT_end downto mDAT_start) <= msend.DAT;
    msend_vect(mSEL_end downto mSEL_start) <= msend.SEL;
    
-   mrecv.WE <= mrecv_vect(mWE_start);
+   mrecv.CYC <= mrecv_vect(mCYC_start);
+   mrecv.WE  <= mrecv_vect(mWE_start);
    mrecv.ADR <= mrecv_vect(mADR_end downto mADR_start);
    mrecv.DAT <= mrecv_vect(mDAT_end downto mDAT_start);
    mrecv.SEL <= mrecv_vect(mSEL_end downto mSEL_start);
@@ -86,9 +91,11 @@ begin
    srecv.DAT <= srecv_vect(sDAT_end downto sDAT_start);
 
    -- Slave clock domain: slave -> mFIFO
-   mw_en <= mw_rdy and sa_rdy and slave_i.CYC and slave_i.STB;
+   mw_en <= (mw_rdy and sa_rdy and slave_i.CYC and slave_i.STB) or 
+            (not slave_i.CYC and slave_CYC); -- Masters may only drop cycle if FIFOs are empty
    sa_en <= mw_rdy and sa_rdy and slave_i.CYC and slave_i.STB;
    slave_o.STALL <= not mw_rdy or not sa_rdy;
+   msend.CYC <= slave_i.CYC;
    msend.ADR <= slave_i.ADR;
    msend.WE  <= slave_i.WE;
    msend.SEL <= slave_i.SEL;
@@ -96,8 +103,8 @@ begin
    
    -- Master clock domain: mFIFO -> master
    mr_en <= mr_rdy and (not master_o_STB or not master_i.STALL);
-   master_o.CYC <= '1';
-   master_o.STB <= master_o_STB;
+   master_o.CYC <= mrecv.CYC;
+   master_o.STB <= master_o_STB; -- is high outside of CYC. that's ok; it should be ignored.
    master_o.ADR <= mrecv.ADR;
    master_o.WE  <= mrecv.WE;
    master_o.SEL <= mrecv.SEL;
@@ -133,8 +140,10 @@ begin
       if rising_edge(slave_clk_i) then
          if rst_n_i = '0' then
             slave_o_PUSH <= '0';
+            slave_CYC <= '0';
          else
             slave_o_PUSH <= sr_en;
+            slave_CYC <= slave_i.CYC;
          end if;
       end if;
    end process;
