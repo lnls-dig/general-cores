e11b2020e1b556a721e23ff88bc78920ddb11b97
Wesley W. Terpstra <w.terpstra@gsi.de>
2013-04-12 11:00:39 +0200

    move platform specific modules into conditional folders

--- modules/wishbone/Manifest.py
+++ modules/wishbone/Manifest.py
@@ -1,27 +1,23 @@
-def __helper():
-  dirs = [
-    "wb_async_bridge",
-    "wb_onewire_master",
-    "wb_i2c_master",
-    "wb_bus_fanout",
-    "wb_dpram",
-    "wb_gpio_port",
-    "wb_simple_timer",
-    "wb_uart",
-    "wb_vic",
-    "wb_spi",
-    "wb_crossbar",
-    "wb_lm32",
-    "wb_slave_adapter",
-    "wb_xilinx_fpga_loader",
-    "wb_clock_crossing",
-    "wb_dma",
-    "wb_serial_lcd",
-    "wbgen2"
-    ]
-  if (target == "altera"): dirs.extend(["wb_pcie"]);
-  return dirs
+modules =  { "local" : [
+  "wb_async_bridge",
+  "wb_onewire_master",
+  "wb_i2c_master",
+  "wb_bus_fanout",
+  "wb_dpram",
+  "wb_gpio_port",
+  "wb_simple_timer",
+  "wb_uart",
+  "wb_vic",
+  "wb_spi",
+  "wb_crossbar",
+  "wb_lm32",
+  "wb_slave_adapter",
+  "wb_clock_crossing",
+  "wb_dma",
+  "wb_serial_lcd",
+  "wbgen2"
+  ]}
 
-modules =  { "local" : __helper() };
-
-files = ["wishbone_pkg.vhd"];
+files = [
+  "wishbone_pkg.vhd"
+  ]
