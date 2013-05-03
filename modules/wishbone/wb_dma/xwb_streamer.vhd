5fec7a370880104050186d9dbb9c5ad3ef3342d1
Wesley W. Terpstra <w.terpstra@gsi.de>
2013-05-03 14:13:13 +0200

    xwb_streamer: reduce to a wrapper of xwb_dma
    The optimizer should be able to remove all the control registers
    as they are unchanging, so nothing is lost.

--- modules/wishbone/wb_dma/xwb_streamer.vhd
+++ modules/wishbone/wb_dma/xwb_streamer.vhd
@@ -26,134 +26,28 @@ entity xwb_streamer is
 end xwb_streamer;
 
 architecture rtl of xwb_streamer is
-  constant ringLen : integer := 2**logRingLen;
-  type ring_t is array (ringLen-1 downto 0) of t_wishbone_data;
-  
-  -- Ring buffer for shipping data from read master to write master
-  signal ring : ring_t;
-  
-  -- State registers (pointer into the ring)
-  -- Invariant: read_issue_offset >= read_result_offset >= write_issue_offset >= write_result_offset
-  --            read_issue_offset - write_result_offset  <= ringLen (*NOT* strict '<')
-  signal read_issue_offset   : unsigned(logRingLen downto 0);
-  signal read_result_offset  : unsigned(logRingLen downto 0);
-  signal write_issue_offset  : unsigned(logRingLen downto 0);
-  signal write_result_offset : unsigned(logRingLen downto 0);
-  
-  -- Registered wishbone control signals
-  signal r_master_o_CYC : std_logic;
-  signal w_master_o_CYC : std_logic;
-  signal r_master_o_STB : std_logic;
-  signal w_master_o_STB : std_logic;
-  
-  function active_high(x : boolean)
-    return std_logic is
-  begin
-    if (x) then
-      return '1';
-    else
-      return '0';
-    end if;
-  end active_high;
-  
-  function index(x : unsigned(logRingLen downto 0))
-    return integer is
-  begin
-    if logRingLen > 0 then
-      return to_integer(x(logRingLen-1 downto 0));
-    else
-      return 0;
-    end if;
-  end index;
-  
+  signal slave : t_wishbone_slave_in;
 begin
-  -- Hard-wired master pins
-  r_master_o.CYC <= r_master_o_CYC;
-  w_master_o.CYC <= w_master_o_CYC;
-  r_master_o.STB <= r_master_o_STB;
-  w_master_o.STB <= w_master_o_STB;
-  r_master_o.ADR <= (others => '0');
-  w_master_o.ADR <= (others => '0');
-  r_master_o.SEL <= (others => '1');
-  w_master_o.SEL <= (others => '1');
-  r_master_o.WE  <= '0';
-  w_master_o.WE  <= '1';
-  r_master_o.DAT <= (others => '0');
-  w_master_o.DAT <= ring(index(write_issue_offset));
-  
-  main : process(clk_i)
-    variable read_issue_progress   : boolean;
-    variable read_result_progress  : boolean;
-    variable write_issue_progress  : boolean;
-    variable write_result_progress : boolean;
-    
-    variable new_read_issue_offset   : unsigned(logRingLen downto 0);
-    variable new_read_result_offset  : unsigned(logRingLen downto 0);
-    variable new_write_issue_offset  : unsigned(logRingLen downto 0);
-    variable new_write_result_offset : unsigned(logRingLen downto 0);
 
-    variable ring_boundary : boolean;
-    variable ring_high     : boolean;
-    variable ring_full     : boolean;
-  begin
-    if (rising_edge(clk_i)) then
-      if (rst_n_i = '0') then
-        read_issue_offset   <= (others => '0');
-        read_result_offset  <= (others => '0');
-        write_issue_offset  <= (others => '0');
-        write_result_offset <= (others => '0');
-        
-        r_master_o_CYC <= '0';
-        w_master_o_CYC <= '0';
-        r_master_o_STB <= '0';
-        w_master_o_STB <= '0';
-      else
-        -- Detect bus progress
-        read_issue_progress   := r_master_o_STB = '1' and r_master_i.STALL = '0';
-        write_issue_progress  := w_master_o_STB = '1' and w_master_i.STALL = '0';
-        read_result_progress  := r_master_o_CYC = '1' and (r_master_i.ACK = '1' or r_master_i.ERR = '1' or r_master_i.RTY = '1');
-        write_result_progress := w_master_o_CYC = '1' and (w_master_i.ACK = '1' or w_master_i.ERR = '1' or w_master_i.RTY = '1');
-        
-        -- Advance read pointers
-        if read_issue_progress then
-          new_read_issue_offset := read_issue_offset + 1;
-        else
-          new_read_issue_offset := read_issue_offset;
-        end if;
-        if read_result_progress then
-          ring(index(read_result_offset)) <= r_master_i.DAT;
-          new_read_result_offset := read_result_offset + 1;
-        else
-          new_read_result_offset := read_result_offset;
-        end if;
-        
-        -- Advance write pointers
-        if write_issue_progress then
-          new_write_issue_offset := write_issue_offset + 1;
-        else
-          new_write_issue_offset := write_issue_offset;
-        end if;
-        if write_result_progress then
-          new_write_result_offset := write_result_offset + 1;
-        else
-          new_write_result_offset := write_result_offset;
-        end if; 
-        
-        ring_boundary := index(new_read_issue_offset) = index(new_write_result_offset);
-        ring_high     := new_read_issue_offset(logRingLen) /= new_write_result_offset(logRingLen);
-        ring_full     := ring_boundary and ring_high;
-        
-        r_master_o_STB <= active_high (not ring_full);
-        r_master_o_CYC <= active_high((not ring_full) or 
-                                      (new_read_result_offset  /= new_read_issue_offset));
-        w_master_o_STB <= active_high (new_write_issue_offset  /= new_read_result_offset);
-        w_master_o_CYC <= active_high (new_write_result_offset /= new_read_result_offset);
-        
-        read_issue_offset   <= new_read_issue_offset;
-        read_result_offset  <= new_read_result_offset;
-        write_issue_offset  <= new_write_issue_offset;
-        write_result_offset <= new_write_result_offset;
-      end if;
-    end if;
-  end process;
+  slave.cyc <= '1';
+  slave.stb <= '1';
+  slave.we  <= '1';
+  slave.adr <= x"00000010"; -- transfer count
+  slave.sel <= (others => '1');
+  slave.dat <= (others => '1');
+  
+  dma: xwb_dma
+    generic map(
+      logRingLen => logRingLen)
+    port map(
+      clk_i       => clk_i,
+      rst_n_i     => rst_n_i,
+      slave_i     => slave,
+      slave_o     => open,
+      r_master_i  => r_master_i,
+      r_master_o  => r_master_o,
+      w_master_i  => w_master_i,
+      w_master_o  => w_master_o,
+      interrupt_o => open);
+
 end rtl;
