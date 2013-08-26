7e96d351d972e0a2b4396b8467e22a827fb383bc
Lucas Russo <lerwys@gmail.com>
2013-08-26 14:11:34 -0300

    modules/*/wb_spi_bidir/*: fix missing files in Manifest.py

--- modules/wishbone/wb_spi_bidir/Manifest.py
+++ modules/wishbone/wb_spi_bidir/Manifest.py
@@ -2,6 +2,8 @@ files = [ "spi_bidir_clgen.v",
           "spi_bidir_defines.v",
           "spi_bidir_shift.v",
           "spi_bidir_top.v",
+	  "wb_spi_bidir.vhd",
+	  "xwb_spi_bidir.vhd",
           "timescale.v",
           "wb_spi_bidir.vhd" ]
 
