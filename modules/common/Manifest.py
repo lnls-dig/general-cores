cc11535bc07bc2b09fb2f895bf71c5187cd6a9e2
Mathias Kreider <mkreider@gsi.de>
2015-11-17 11:19:42 +0100

    Added package for bit matrix manipulation

--- modules/common/Manifest.py
+++ modules/common/Manifest.py
@@ -1,4 +1,5 @@
-files = [	"gencores_pkg.vhd",
+files = [	"matrix_pkg.vhd",
+		"gencores_pkg.vhd",
                 "gc_crc_gen.vhd",
                 "gc_moving_average.vhd",
                 "gc_extend_pulse.vhd",
