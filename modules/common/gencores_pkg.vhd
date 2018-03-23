0118fda68a7aeec65350bd0c5762bbcd0b8b66df
Dimitris Lampridis <Dimitris.Lampridis@cern.ch>
2018-03-23 14:19:10 +0100

    hdl: import f_pick functions from mockturtle

--- modules/common/gencores_pkg.vhd
+++ modules/common/gencores_pkg.vhd
@@ -611,6 +611,16 @@ package gencores_pkg is
   function f_to_std_logic_vector (c : character) return std_logic_vector;
   function f_to_std_logic_vector (s : string) return std_logic_vector;
 
+  -- Functions for short-hand if assignments
+  function f_pick (cond : boolean; if_1 : std_logic; if_0 : std_logic)
+    return std_logic;
+  function f_pick (cond : boolean; if_1 : std_logic_vector; if_0 : std_logic_vector)
+    return std_logic_vector;
+  function f_pick (cond : std_logic; if_1 : std_logic; if_0 : std_logic)
+    return std_logic;
+  function f_pick (cond : std_logic; if_1 : std_logic_vector; if_0 : std_logic_vector)
+    return std_logic_vector;
+
 end package;
 
 package body gencores_pkg is
@@ -801,5 +811,44 @@ package body gencores_pkg is
     end loop;
     return rv;
   end function f_to_std_logic_vector;
+
+  ------------------------------------------------------------------------------
+  -- Functions for short-hand if assignments
+  ------------------------------------------------------------------------------
+  function f_pick (cond : std_logic; if_1 : std_logic; if_0 : std_logic)
+    return std_logic
+  is
+  begin
+    if cond = '1' then
+      return if_1;
+    else
+      return if_0;
+    end if;
+  end function f_pick;
+
+  function f_pick (cond : std_logic; if_1 : std_logic_vector; if_0 : std_logic_vector)
+    return std_logic_vector
+  is
+  begin
+    if cond = '1' then
+      return if_1;
+    else
+      return if_0;
+    end if;
+  end function f_pick;
+
+  function f_pick (cond : boolean; if_1 : std_logic; if_0 : std_logic)
+    return std_logic
+  is
+  begin
+    return f_pick (f_to_std_logic(cond), if_1, if_0);
+  end function f_pick;
+
+  function f_pick (cond : boolean; if_1 : std_logic_vector; if_0 : std_logic_vector)
+    return std_logic_vector
+  is
+  begin
+    return f_pick (f_to_std_logic(cond), if_1, if_0);
+  end function f_pick;
 
 end gencores_pkg;
