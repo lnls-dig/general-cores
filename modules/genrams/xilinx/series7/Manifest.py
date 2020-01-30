200062d6e0c84fc0005efad3618edb32eb440ed9
Lucas Russo <lerwys@gmail.com>
2020-01-30 17:48:41 -0200

    modules/genrams/*/series7: add wrappers for dual rst FIFO
    This was taken from virtex6 and modified accordingly.

--- modules/genrams/xilinx/series7/Manifest.py
+++ modules/genrams/xilinx/series7/Manifest.py
@@ -1,1 +1,5 @@
-files =["s7_fifo_pkg.vhd", "s7_hwfifo_wrapper.vhd", "generic_async_fifo.vhd", "generic_sync_fifo.vhd"];
+files =["s7_fifo_pkg.vhd",
+        "s7_hwfifo_wrapper.vhd",
+        "generic_async_fifo.vhd",
+        "generic_async_fifo_dual_rst.vhd",
+        "generic_sync_fifo.vhd"]
