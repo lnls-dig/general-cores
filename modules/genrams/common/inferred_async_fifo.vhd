66df5fa6905d3fb28f8519e5dd82d7a30b99034f
Tomasz Wlostowski <tomasz.wlostowski@cern.ch>
2021-12-16 16:38:59 +0100

    inferred_async_fifo: avoid nested 'others' clause in signal initialization to keep ISE (Virtex5) happy

--- modules/genrams/common/inferred_async_fifo.vhd
+++ modules/genrams/common/inferred_async_fifo.vhd
@@ -116,11 +116,20 @@ architecture syn of inferred_async_fifo is
     bin_x, gray_x   : t_counter;
   end record;
 
+  constant c_counters_reset_value : t_counter_block :=
+    ( bin => (others => '0'),
+      bin_next => (others => '0'),
+      gray  => (others => '0'),
+      gray_next  => (others => '0'),
+      bin_x => (others => '0'),
+      gray_x  => (others => '0') );
+  
+
   type t_mem_type is array (0 to g_size-1) of std_logic_vector(g_data_width-1 downto 0);
   signal mem : t_mem_type := (others => (others => '0'));
 
-  signal rcb, wcb : t_counter_block := (others =>(others => '0'));
-
+  signal rcb, wcb : t_counter_block := c_counters_reset_value;
+  
   signal full_int, empty_int               : std_logic;
   signal almost_full_int, almost_empty_int : std_logic;
   signal going_full                        : std_logic;
